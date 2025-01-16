34cfb5b62af32b7c6c9e29a847411c45258f5fa8
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-01-16 14:21:04 +0530

    Added tip tap editor

--- src/components/NotePicker.tsx
+++ src/components/NotePicker.tsx
@@ -9,13 +9,17 @@ const NotePicker = () => {
     setContent(value);
   };
   return (
-    <form className="max-w-3xl mx-auto w-full grid place-items-center pt-10 mb-10">
-      <div className="text-3xl text-white text-center mb-10">Notes Picker</div>
-      <TipTap
-        content={content}
-        onChange={(value: string) => handleContentChange(value)}
-      />
-    </form>
+    <>
+      <form className="max-w-3xl mx-auto w-full  grid place-items-center pt-10 mb-10">
+        <div className="text-3xl text-white text-center mb-10">
+          Notes Picker
+        </div>
+        <TipTap
+          content={content}
+          onChange={(value: string) => handleContentChange(value)}
+        />
+      </form>
+    </>
   );
 };
 
--- src/components/TipTap.tsx
+++ src/components/TipTap.tsx
@@ -1,17 +1,43 @@
-"use client"
-import { EditorContent, useEditor } from "@tiptap/react";
+"use client";
+import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import React, { FC } from "react";
+import Underline from "@tiptap/extension-underline";
+import Toolbar from "./ToolsBar";
+import BulletList from "@tiptap/extension-bullet-list";
+import ListItem from "@tiptap/extension-list-item";
+import OrderedList from "@tiptap/extension-ordered-list";
+import { FC } from "react";
 
-const TipTap: FC<{
-  content: string;
+const Tiptap: FC<{
   onChange: (value: string) => void;
-}> = ({ content, onChange }) => {
+  content: string;
+}> = ({ onChange, content }) => {
+  const handleChange = (newContent: string) => {
+    onChange(newContent);
+  };
   const editor = useEditor({
-    extensions: [StarterKit],
-    content,
+    extensions: [StarterKit, Underline, BulletList, OrderedList, ListItem],
+    editorProps: {
+      attributes: {
+        class:
+          "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 text-gray-400 items-start w-full gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none",
+      },
+    },
+    onUpdate: ({ editor }) => {
+      handleChange(editor.getHTML());
+    },
   });
-  return <EditorContent editor={editor} />;
+
+  return (
+    <div className="w-full px-4">
+      <Toolbar editor={editor} content={content} />
+      <EditorContent
+        style={{ whiteSpace: "pre-line" }}
+        editor={editor}
+        className="list-disc list-inside"
+      />
+    </div>
+  );
 };
 
-export default TipTap;
+export default Tiptap;
